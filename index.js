cab8919ab6282dc5469cd1ddef23d254cd0ca7c4
Michael Amprimo <hello@michaelamprimo.com>
2017-11-03 18:24:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const icons = {};
 
 data.icons.forEach(i => {
-  i.svg = fs.readFileSync(`${__dirname}/icons/${i.file}`, 'utf8');
+  i.svg = fs.readFileSync(`${__dirname}/svg/${i.file}`, 'utf8');
   icons[i.name] = i
 });
 
